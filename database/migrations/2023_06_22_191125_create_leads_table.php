dea3d25cb6e6ad54702a6bac7ddee41d78ad5c6a
Clayton Ramalho Silva <cdston@gmail.com>
2023-06-22 17:01:32 -0300

    feature: implementando layout

---
+++ database/migrations/2023_06_22_191125_create_leads_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('value',8,2)->default(0);
+            $table->string('name_customer')->nullable();
+            $table->string('product')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
